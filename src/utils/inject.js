f6b1c1234dcb9c41272eba613236eeb68ebb2ee7
Mike Hoffert <mike.hoffert@gmail.com>
2018-09-23 09:54:22 -0600

    Fix isArray function

--- src/utils/inject.js
+++ src/utils/inject.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 export default function(elements, component, props) {
-  if(!elements.isArray) {
+  if(Array.isArray(elements) !== true) {
     elements = [].push(elements);
   }
 
