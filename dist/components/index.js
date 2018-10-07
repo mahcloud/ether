6a34bf1516eccf315ba9aad60b2e16bfa50d793f
Mike Hoffert <mike.hoffert@gmail.com>
2018-10-07 16:37:24 -0600

    Dist files

--- dist/components/index.js
+++ dist/components/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Ellipsis = exports.Button = undefined;
+exports.Notice = exports.Ellipsis = exports.Button = undefined;
 
 var _button = require("./button");
 
@@ -13,7 +13,12 @@ var _ellipsis = require("./ellipsis");
 
 var _ellipsis2 = _interopRequireDefault(_ellipsis);
 
+var _notice = require("./notice");
+
+var _notice2 = _interopRequireDefault(_notice);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.Button = _button2.default;
 exports.Ellipsis = _ellipsis2.default;
+exports.Notice = _notice2.default;
